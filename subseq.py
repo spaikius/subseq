6a03d6ba3804cf448666787a59f8c60859a895a1
spaikius <35471348+spaikius@users.noreply.github.com>
2018-02-28 18:04:30 +0200

    programa pasiima sekos vienraidi aa koda, pozicija ir grandine

--- subseq.py
+++ subseq.py
@@ -1,9 +1,24 @@
 from pymol import cmd
 
-def subseq():
+cmd.set("seq_view", 1)
 
-    aaList = {'aa': []}
-    cmd.iterate("name ca", "aa.append([resi, chain, resn])", space=aaList)
-    print aaList
+oneLetter = {
+	'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
+	'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',
+	'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
+	'ALA': 'A', 'VAL': 'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'
+}
+
+def subseq():
+	# Gaunam bendros sekos aa, pozicija, grandine
+	aaList = {'aa': []}
+	cmd.iterate("name ca", "aa.append([resn, resi, chain])", space=aaList)
+
+	# Mums reikia vienraidzio aa kodo, o pymol grazina triraidi koda
+	for aa in aaList['aa']: aa[0] = oneLetter[aa[0]]
+
+	print aaList
+	
+	
 
 cmd.extend('subseq', subseq)
