7d742059568e7910f5477398dc6f8b77633998d2
spaikius <35471348+spaikius@users.noreply.github.com>
2018-03-13 01:20:05 +0200

    module for making pymol selections

---
+++ susbseq/ss_pymol_SELECT.py
@@ -0,0 +1,31 @@
+# @module ss_pymol_select.py
+# @public functions: select
+# @public variables: -
+# @private functions: -
+# @private variables: -
+
+from pymol import cmd
+from random import randint
+
+
+# @function type: public
+# @arguments: list from ss_RE_SEARCH.subseq_re() or ss_LA_SEARCH.subseq_la()
+# @returns: -
+# @description: for given data creates a 'ss_%{randint}' selection and fills it with residues
+def select(_list_of_tuple):
+    select_list = _list_of_tuple
+    lower_bound = 0
+    upper_bound = 10000
+    # select ID
+    select_name = 'ss_' + str(randint(lower_bound, upper_bound))
+    # empty select
+    cmd.select(select_name, None) 
+
+    for sele_tuple in select_list:
+        model = sele_tuple[0]
+        chain = sele_tuple[1]
+        start_id = sele_tuple[2]
+        end_id = sele_tuple[3]
+        # select /model/?/chain/resi
+
+        cmd.select(select_name, select_name +  " | /" + str(model) + "//" + str(chain) + "/" + str(start_id) + "-" + str(end_id))
