a3a9e8574b1db766e62c19118bd2062173952a4d
spaikius <35471348+spaikius@users.noreply.github.com>
2018-03-12 17:58:50 +0200

    Bug fix

--- susbseq/ss_DATA.py
+++ susbseq/ss_DATA.py
@@ -1,15 +1,16 @@
 # @module ss_DATA.py
-# @public functions: get_data,
-#                    del_data
+# @public functions: get_data
 # @public variables: -
 # @private functions: _init_data,
-#                     _filter_data,
-#                     _construct_aa_chains
-# @private variables: _aa_dict, _complete_chains
+#                     _replace_to_one_letter,
+#                     _create_empty_dict,
+#                     _fill_dict,
+#                     _get_chains
+# @private variables: _one_letter
 
 from pymol import cmd
 
-_oneLetter = {
+_one_letter = {
     'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
     'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',
     'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
@@ -17,74 +18,112 @@ _oneLetter = {
     'CA': ''
 }
 
-_aa_dict = dict()
-_complete_chains = dict()
-
 
 # @function type: public
-# @arguments: model list, chain list
+# @arguments: model list
 # @returns: nested dictionary {model: {chain: sequence, },  }
 # @description: encapsulation, returns nested dictionary
 def get_data(_models, _chains):
-    _init_data()
-    _filter_data(_models, _chains)
-    _construct_aa_chains(_models, _chains)
+    aa_dict = _init_data() # get all data from pymol
+    _replace_to_one_letter(aa_dict) # replace all 3 letters
+    complete_chains = _create_empty_dict(_models, _chains) # creates an empty dict for complete_chains
+    _fill_dict(_models, _chains, complete_chains, aa_dict) # using pymol data fills complete_chains with data
 
-    return _complete_chains
-
-
-# @function type: public
-# @arguments: -
-# @returns: -
-# @description: frees memory
-def del_data():
-    try:
-        del _aa_dict
-        del _complete_chains
-    except NameError:
-        return
+    return complete_chains
 
 
 # @function type: private
 # @arguments: -
-# @returns: -
-# @description: fills _aa_dict{[...]} with information about residue 
+# @returns: dict
+# @description: return aa_dict{[...]} with information about residue 
 def _init_data():
-    _aa_dict['aa_list'] = list()
-    # iterets through all c-alpha atoms and returns
-    # possition number, three letter code, coresponding chain and model
+    aa_dict = dict()
+    aa_dict['aa_list'] = list()
+    # iterate through all c-alpha atoms and return
+    # position number, three letter code, corresponding chain and model
     cmd.iterate("name ca",
                 "aa_list.append([resn, resi, chain, model])",
-                space=_aa_dict)
+                space=aa_dict)
+
+    return aa_dict
 
 
 # @function type: private
-# @arguments: model list, chain list
+# @arguments: -
 # @returns: -
-# @description: filters _aa_dict 
-def _filter_data(_models, _chains):
-    aa_list = _aa_dict['aa_list']
-
-    for i in range(0, len(aa_list)):
-        if aa_list[i][3] not in _models or \
-                        aa_list[i][2] not in _chains:
-            # delete list element
-            del _aa_dict['aa_list'][i]
+# @description: replaces all 3 letters code aa to single letter code aa
+def _replace_to_one_letter(_aa_dict):
+    for aa_list in _aa_dict['aa_list']:
+        aa_list[0] = _one_letter[aa_list[0]]
 
 
 # @function type: private
-# @arguments: model list, chain list
+# @arguments: -
 # @returns: -
-# @description: constructs a nested dictionary _complete_chains 
-# {model: {chain: sequence, },  }
-def _construct_aa_chains(_models, _chains):
-    for aa in _aa_dict['aa_list']:
-        aa[0] = _oneLetter[aa[0]]
+# @description: constructs an empty nested dictionary _complete_chains
+# {model: {chain: {sequence: , start: , end: , }, },  }
+def _create_empty_dict(_models, _chains):
+    complete_chains = dict()
+    sequence = 'sequence'
+    start = 'start'
+    end = 'end'
 
     for model in _models:
-        _complete_chains[model] = dict()
-        for chain in _chains:
-            _complete_chains[model][chain] = ''
+        complete_chains[model] = dict()
 
-    for i in _aa_dict['aa_list']:
-        _complete_chains[i[3]][i[2]] += str(i[0])
+        for chain in _get_chains(model):
+            # skip if chain is not requested
+            if chain not in _chains:
+                continue
+
+            complete_chains[model][chain] = dict()
+            complete_chains[model][chain][sequence] = ''
+            complete_chains[model][chain][start] = ''
+            complete_chains[model][chain][end] = ''
+
+    return complete_chains
+
+
+# @function type: private
+# @arguments: -
+# @returns: -
+# @description: constructs a nested dictionary _complete_chains 
+# {model: {chain: {sequence: , start: , end: , }, },  }
+# warns if any empty model is presented (does not contain any chain from users requested chains)
+def _fill_dict(_models, _chains, _complete_chains, _aa_dict):
+    sequence = 'sequence'
+    start = 'start'
+    end = 'end'
+
+    for resn, resi, chain, model in _aa_dict['aa_list']:
+        # if model or chain is not requested, skip iteration
+        if model not in _models or chain not in _chains:
+            continue
+
+        _complete_chains[model][chain][sequence] += resn
+
+        if len(_complete_chains[model][chain][sequence]) == 1:
+            _complete_chains[model][chain][start] = int(resi)
+            _complete_chains[model][chain][end] = int(resi)
+        else:
+            _complete_chains[model][chain][end] += 1
+
+    # remove all empty models and warn if found any 
+    for model in _complete_chains.keys():
+        if not _complete_chains[model]:
+            print(" Warning: model '" + str(model) + "' does not contain any requested chain\n" +
+                "  Models '" + str(model) + "' chains: " + str(_get_chains(model)) + '\n' +
+                "  Requested chains: " + str(_chains))
+            del _complete_chains[model]
+
+
+# @function type: private
+# @arguments: a list of models
+# @returns: a list of chains with no duplicates
+# @description: returns a list of chains in model
+def _get_chains(_model):
+    chain_list = list()
+    for chain in cmd.get_chains(_model):
+            chain_list.append(chain)
+
+    return chain_list
