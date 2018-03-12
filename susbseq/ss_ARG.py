4c100d0cfe58b9e17a275a280fa9e59ea247dee7
spaikius <35471348+spaikius@users.noreply.github.com>
2018-03-12 17:56:22 +0200

    Bug fix

--- susbseq/ss_ARG.py
+++ susbseq/ss_ARG.py
@@ -5,13 +5,15 @@
 # @public variables: -
 # @private functions: _set_parameters,
 #                     _validate_parameters,
-#                     _get_modules
-#                     _get_chains
-# @private variables: _type_values, _params
+#                     _parse_str_to_list,
+#                     _get_all_modules,
+#                     _get_all_chains
+# @private variables: _algorithm_type_values, _params
 
 from pymol import cmd
+import re
 
-_type_values = ['re',  # Regular Expresion (default)
+_algorithm_type_values = ['re',  # Regular Expresion (default)
                 'la'   # Local alignment
                 ]
 
@@ -56,7 +58,6 @@ def reset_parameters():
 def _set_parameters(_kwargs):
     # Remove '_self' key if presented
     _kwargs.pop('_self', None)
-
     for key, value in _kwargs.items():
         key = key.lower()
 
@@ -65,7 +66,7 @@ def _set_parameters(_kwargs):
         if key in _params:
             _params[key] = value
         else:
-            raise Exception('Unknown parameter: ', key)
+            raise Exception('Unknown parameter: ' + str(key))
 
 
 # @functions type: private
@@ -75,61 +76,59 @@ def _set_parameters(_kwargs):
 # If key value is not provided, it will set the default value
 def _validate_parameters():
     algorithm_type = _params['type']
+    default_algo_type = _algorithm_type_values[0]
     target = _params['target']
     models = _params['models']
     chains = _params['chains']
 
-    pymol_models = _get_models()
-    pymol_models_chains = _get_chains(pymol_models)
+    pymol_models = _get_all_models()
+    pymol_models_chains = _get_all_chains(pymol_models)
 
+    #-- Algorithm type validation
     if algorithm_type is None:
-        # default value: re
-        _params['type'] = _type_values[0]
+        _params['type'] = default_algo_type
     else:
-        if algorithm_type not in _type_values:
-            raise Exception("Unknown 'type' value: ", _params['type'])
+        if algorithm_type not in _algorithm_type_values:
+            raise Exception("Undefined algorithm type: " + str(algorithm_type))
 
-    # --
+    #-- Target validation
     if target is None:
-        raise Exception("Parameter 'target' must be defined")
-    elif not isinstance(target, str):
-        raise Exception("Parameter 'target' bust be type of 'str'")
+        raise Exception("Parameter 'target=' must be defined")
 
-    # --
+    #-- Models validation
     if models is None:
-        # default value: all existing models
-        _params['models'] = _get_models()
-    # only 1 model is given
-    elif isinstance(models, str):
-        # check if it exists
-        if models not in pymol_models:
-            raise Exception('Model: ', models, ' is undefined')
-    # a list of models is given
-    elif isinstance(models, list):
-        for model in models:
-            # check if model type is str
-            if not isinstance(model, str):
-                raise Exception('Models value can not be a nested list')
-            # check if model exists
-            if model not in pymol_models:
-                raise Exception('Models: ', models, ' are undefined')
-    # the given object is not a type of str or list
+        _params['models'] = pymol_models
     else:
-        raise Exception("models value must be type of 'str' or 'list'")
+        models_list = _parse_str_to_list(models)
+        for model in models_list:
+            if model not in pymol_models:
+                raise Exception("Model '" + str(model) + "' is not available\n" +
+                    "Available models: " + str(pymol_models))
 
-    # --
+        _params['models'] = models_list
+
+    #-- Chains validation
     if chains is None:
-        # default value: all existing chains
         _params['chains'] = pymol_models_chains
     else:
-        if isinstance(chains, list):
-            required_chains = _get_chains(models)
-            for chain in chains:
-                if chain not in required_chains:
-                    raise Exception('Chain: ', chain, 'can not be',
-                                    ' found in: ', models)
-        else:
-            raise Exception("chains value must be type of 'list'")
+        chains_list = _parse_str_to_list(chains)
+        for chain in chains_list:
+            if chain not in pymol_models_chains:
+                raise Exception("Chain '" + str(chain) + "' is not available\n" +
+                    "Available chains: " + str(pymol_models_chains))
+
+        _params['chains'] = chains_list
+
+# @function type: private
+# @arguments: string
+# @returns: list
+# @descriptions: parses string from _kwargs values and returns list 
+def _parse_str_to_list(_str):
+    # split by any seperator (, . / etc.)
+    raw_str = re.sub('([A-Za-z0-9_]+)', r'\1', _str)
+    # remove symbols '[', ']', ''', '"', white spaces
+    raw_str = re.sub('[\[\]\'\"\s]', '', raw_str)
+    return raw_str.split(',')
 
 
 # @function type: private
@@ -137,7 +136,7 @@ def _validate_parameters():
 # @returns: a list of models
 # @descriptions: returns names list of all existing models in pymol
 # raises an exception if no model was found
-def _get_models():
+def _get_all_models():
     models = cmd.get_names()
 
     if not bool(models):
@@ -151,7 +150,7 @@ def _get_models():
 # @returns: a list of chains with no duplicates
 # @description: gets all chains in provided models and returns a list
 # without duplicates
-def _get_chains(_models):
+def _get_all_chains(_models):
     chain_list = list()
     for model in _models:
         for chain in cmd.get_chains(model):
