1ae7f063b35b0df7f5b7e85d66b3ebf4165b6272
Rimvydas <rimvydas.nore@gmail.com>
2018-03-17 12:37:59 +0200

    Extending for Local Aligment search

---
+++ subseq/ss_LA_SEARCH.py
@@ -0,0 +1,20 @@
+# @module ss_LA_SEARCH.py
+# @public functions: -
+# @public variables: -
+# @private functions: -
+# @private variables: -
+
+
+def subseq_la(_target, _data,_matrix , _gap_cost, _extend_cost, _match_value):
+    target = _target
+    data = _data
+    matrix = _matrix
+    gap_cost = int(_gap_cost)
+    extend_cost = int(_extend_cost)
+    match_value = int(_match_value)
+
+    for model in data.keys():
+        for chain in data[model].keys():
+            _SWA(target, model, matrix, gap_cost, extend_cost, match_value)
+
+def _SWA():
