8b679e9aa5fc16c229f8aceb95ca8bc8f941e4ee
Rimvydas <rimvydas.nore@gmail.com>
2018-03-17 13:58:44 +0200

    header/matrix methods added

--- subseq/ss_LA_SEARCH.py
+++ subseq/ss_LA_SEARCH.py
@@ -15,9 +15,22 @@ def subseq_la(_target, _data, _matrix, _gap_cost, _extend_cost, _match_value):
     extend_cost = int(_extend_cost)
     match_value = int(_match_value)
 
+    target_header = _create_header_list(target)
+
     for model in data.keys():
         for chain in data[model].keys():
-            _SWA(target, model, chain, matrix, gap_cost, extend_cost, match_value)
+        	chain_header = _create_header_list(chain)
+        	score_matrix = _create_score_matrix(target, chain)
+
+            # _SWA()
+
+
+def _create_score_matrix(_target, _chain):
+	return np.zeros(shape=[len(_target), len(_chain)])
+
+
+def _create_header_list(_seq):
+	return [i for i in _seq.upper()]
 
 
 def _SWA():
