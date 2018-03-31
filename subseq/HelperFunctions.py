22cdbbf83f858d139a988a2189e7990fee89ea23
Noreika Rimvydas <rimvydas.noreika@mif.stud.vu.lt>
2018-03-31 20:06:04 +0300

    bug fix

--- subseq/HelperFunctions.py
+++ subseq/HelperFunctions.py
@@ -12,8 +12,8 @@ import re
 from pymol import cmd
 
 def get_all_models():
-	""" return all pymol models """
-
+    """ return all pymol models """
+    
     models = cmd.get_names()
 
     # if a list is empty -> no models are currently avialable
@@ -24,7 +24,7 @@ def get_all_models():
   
 
 def get_all_chains(models):
-	""" return a list of chains for given models """
+    """ return a list of chains for given models """
 
     chain_list = list()
 
@@ -36,7 +36,7 @@ def get_all_chains(models):
 
 
 def get_chains(model):
-	""" return a list of chains for given only one model """
+    """ return a list of chains for given only one model """
 
     chain_list = list()
     for chain in cmd.get_chains(model):
@@ -46,7 +46,7 @@ def get_chains(model):
 
 
 def parse_str_to_list(string):
-	""" Parse a string and return a list of values """
+    """ Parse a string and return a list of values """
 
     # split by any seperator (, . / etc.) excluding all letters, numbers and _
     raw_str = re.sub('([A-Za-z0-9_]+)', r'\1', string)
