c3c3d077a63a89ad035580cd69355d042a6f7ad8
Noreika Rimvydas <rimvydas.noreika@mif.stud.vu.lt>
2018-03-31 20:09:07 +0300

    bug fix

--- subseq/__init__.py
+++ subseq/__init__.py
@@ -25,7 +25,7 @@ def subseq(*_argv, **_kwargs):
         if parameters['algorithm'] == 're':
             search_result = RegExSearch.subseq_re(parameters['target'], data)
         elif parameters['algorithm'] == 'la':
-            search_result = LocalAlignSearch.subseq_la(parameters['algorithm'], data, parameters['submatrix'], parameters['gapcost'], '2')
+            search_result = LocalAlignSearch.subseq_la(parameters['target'], data, parameters['submatrix'], parameters['gapcost'])
         
         if search_result is not None:
             Select.select(search_result)
@@ -33,6 +33,20 @@ def subseq(*_argv, **_kwargs):
             print("Empty")
     except Exception as e:
         print(' Error: ' + str(e))
+    # parameters = ParseKwargs.ParseKwargs(_kwargs)
+
+    # data = Data.Data(parameters['models'], parameters['chains'])
+
+    # search_result = None
+    # if parameters['algorithm'] == 're':
+    #     search_result = RegExSearch.subseq_re(parameters['target'], data)
+    # elif parameters['algorithm'] == 'la':
+    #     search_result = LocalAlignSearch.subseq_la(parameters['target'], data, parameters['submatrix'], parameters['gapcost'])
+    
+    # if search_result is not None:
+    #     Select.select(search_result)
+    # else:
+    #     print("Empty")
 
 
 cmd.extend('subseq', subseq)
