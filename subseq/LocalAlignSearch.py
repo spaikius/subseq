9a5071ac0b3c5ea534a9752b0f07d965415e9c4b
Noreika Rimvydas <rimvydas.noreika@mif.stud.vu.lt>
2018-04-01 13:06:24 +0300

    bug fix

--- subseq/LocalAlignSearch.py
+++ subseq/LocalAlignSearch.py
@@ -10,6 +10,7 @@ import SmithWaterman
 
 def subseq_la(target, data, matrix, gap_cost, minscore):
     sub_matrix = SubMatrix.SubMatrix(matrix)
+    match_list = list()
 
     for model in data.keys():
         for chain in data[model].keys():
@@ -19,11 +20,22 @@ def subseq_la(target, data, matrix, gap_cost, minscore):
                                                    , gap_cost
                                                    , sub_matrix
                                                    , minscore)
-            print("\n---Aligment---")
-            print("Model: {0}, chain: {1}".format(model, chain))
-            print("Target length: {0} {1}".format(len(target), target[0:40]))
-            print("Subject length: {0} {1}".format(len(sequence), sequence[0:40]))
 
-            aligment.print_data()
+            aligment_list = aligment.get_alignment_coordinates()
 
-            print("\n---END---")
+            if len(aligment_list) != 0:
+                for x, y in aligment_list:
+                    start_id = data[model][chain]['ids'][x]
+                    end_id = data[model][chain]['ids'][y]
+                    match_list.append((model, chain, start_id, end_id))
+
+                print("\n--- Alignment ---")
+                print("Model: {0}, chain: {1}".format(model, chain))
+                print("Target length: {0} {1}".format(len(target), target[0:40]))
+                print("Subject length: {0} {1}".format(len(sequence), sequence[0:40]))
+
+                aligment.print_data()
+
+                print("\n--- END {0} {1}---".format(model, chain))
+
+    return match_list if len(match_list) != 0 else None
