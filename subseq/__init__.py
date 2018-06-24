6d5fcabdd0542daaae262d84fdf0b94be0ec13ca
Noreika Rimvydas <rimvydas.noreika@mif.stud.vu.lt>
2018-06-24 11:31:18 +0300

    modules imports are in init func.

--- subseq/__init__.py
+++ subseq/__init__.py
@@ -3,15 +3,30 @@ import os
 
 from pymol import cmd
 
-sys.path.append(os.path.dirname(__file__))
-
-import subseq_re
-import subseq_local_alignment
-import subseq_global_alignment
 
 def __init__(self):
+    # Append path
+    sys.path.append(os.path.dirname(__file__))
+    
+    # Module files
+    import subseq_re
+    import subseq_local_alignment
+    import subseq_global_alignment
+
+
+def subseq_re_dialog(app):
     pass
 
+
+def subseq_local_alignment_dialog(app):
+    pass
+
+
+def subseq_global_alignment_dialog(app):
+    pass
+
+
+# PyMOL console extends
 cmd.extend('subseq', subseq_re.subseq_re)
 cmd.extend('subseq.local', subseq_local_alignment.subseq_local_alignment)
 cmd.extend('subseq.global', subseq_global_alignment.subseq_global_alignment)
